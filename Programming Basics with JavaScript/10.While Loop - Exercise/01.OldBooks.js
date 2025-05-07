5e6a4b2b53c10a46609fd3f327a2fdc68eacf2ef
penevmtv <penevmtv@abv.bg>
2025-05-07 18:25:33 +0300

    update

--- Programming Basics with JavaScript/10.While Loop - Exercise/01.OldBooks.js
+++ Programming Basics with JavaScript/10.While Loop - Exercise/01.OldBooks.js
@@ -1,1 +1,21 @@
-
+function solve(arr) {
+  let searchBook = arr[0];
+  let checkedBooks = 0;
+  let isFound = false;
+  let index = 1;
+  while (arr[index] !== `No More Books`) {
+    let currentBook = arr[index];
+    if (currentBook === searchBook) {
+      console.log(`You checked ${checkedBooks} books and found it.`);
+      isFound = true; 
+      break;
+    } else { 
+      checkedBooks++;
+      index++;
+    }
+  }
+  if (!isFound) { 
+  console.log(`The book you search is not here!`);
+  console.log(`You checked ${checkedBooks} books.`);
+  }
+}
---
+++ Programming Basics with JavaScript/10.While Loop - Exercise/02.ExamPreparation.js
@@ -0,0 +1,30 @@
+function solve(arr) {
+  let nBadGrades = Number(arr[0]);
+  let badGrades = 0;
+  let sumGrades = 0;
+  let nProblems = 0;
+  let isNeedBreak = false;
+  let index = 1;
+  let problem = ``;
+  while (arr[index] !== `Enough`) {
+    problem = arr[index];
+    nProblems++;
+    let currentGrade = Number(arr[index + 1]);
+    sumGrades += currentGrade;
+    if (currentGrade <= 4) {
+      badGrades++;
+      if (badGrades === nBadGrades) {
+        isNeedBreak = true;
+        break;
+      }
+    }
+    index += 2;
+  }
+  let averageGrade = sumGrades / nProblems;
+  if (!isNeedBreak) {
+    console.log(`Average score: ${averageGrade.toFixed(2)}`);
+    console.log(`Number of problems: ${nProblems}`);
+    console.log(`Last problem: ${problem}`);
+  } else 
+    console.log(`You need a break, ${nBadGrades} poor grades.`);
+}
