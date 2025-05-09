4802dda0caf6387199736642d75a84918d3f815a
penevmtv <penevmtv@abv.bg>
2025-05-09 12:36:39 +0300

    com

---
+++ Programming Basics with JavaScript/11.Nested Loops - Lab/02.MultiplicationTable.js
@@ -0,0 +1,9 @@
+function solve() {
+    for (let n1 = 1; n1 <= 10; n1++) {
+        for (let n2 = 1; n2 <= 10; n2++) {
+            let result = n1 * n2;
+            console.log(`${n1} * ${n2} = ${result}`);
+            
+        }
+    }
+}
