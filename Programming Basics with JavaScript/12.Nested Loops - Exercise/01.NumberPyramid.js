3e47af8b9605c7a096a172517d1f562babcae445
penevmtv <penevmtv@abv.bg>
2025-05-10 18:03:12 +0300

    update 01.NumberPyramid.js

--- Programming Basics with JavaScript/12.Nested Loops - Exercise/01.NumberPyramid.js
+++ Programming Basics with JavaScript/12.Nested Loops - Exercise/01.NumberPyramid.js
@@ -1,1 +1,12 @@
-
+function solve(number) {
+  for (let rows = 1; rows <= number; rows++) {
+    let printRow = ` `;
+    let currentNum = 0;
+    for (let place = 1; ;place++) {
+      printRow += `${currentNum} `;
+      if (place === rows) {
+        
+      }
+    }
+  }
+}
