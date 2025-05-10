e71a8efccb699569289e416345e19ffc6ce0c26b
penevmtv <penevmtv@abv.bg>
2025-05-10 23:56:49 +0300

    com

--- Programming Basics with JavaScript/12.Nested Loops - Exercise/03.SumPrimeNonPrime.js
+++ Programming Basics with JavaScript/12.Nested Loops - Exercise/03.SumPrimeNonPrime.js
@@ -1,6 +1,27 @@
 function solve(arr) {
     let index = 0;
+    let sumPrimeNums = 0;
+    let sumNonPrimeNums = 0;
+    let isPrime = true;
     while (arr[index] !== `stop`) {
-        
+        let currentNum = Number(arr[index]);
+        if (currentNum < 0) {
+            console.log(`Number is negative.`);
+        } else {
+            for (let i = 2; i < currentNum; i++) {
+                if (currentNum % i === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) 
+                sumPrimeNums += currentNum;
+            else
+                sumNonPrimeNums += currentNum;
+            isPrime = true;
+        }
+        index++;
     }
+    console.log(`Sum of all prime numbers is: ${sumPrimeNums}`);
+    console.log(`Sum of all non prime numbers is: ${sumNonPrimeNums}`);
 }
