ce08db2b0002c4de4a443635241d70875cd51800
penevmtv <penevmtv@abv.bg>
2025-05-13 16:17:37 +0300

    com

--- More Exercises/vetParking.js
+++ More Exercises/vetParking.js
@@ -1,1 +1,17 @@
-
+function solve(days, hoursPerDay) {
+    let price = 0;
+    for (let currentDay = 1; currentDay <= days; currentDay++) {
+        let priceForCurrentDay = 0;
+        for (let currentHour = 1; currentHour <= hoursPerDay; currentHour++) {
+            if (currentDay % 2 === 0 && currentHour % 2 !== 0) 
+                priceForCurrentDay += 2.50;
+            else if (currentDay % 2 !== 0 && currentHour % 2 === 0)
+                priceForCurrentDay += 1.25;
+            else
+                priceForCurrentDay += 1;
+        }
+        console.log(`Day: ${currentDay} - ${priceForCurrentDay.toFixed(2)} leva`);
+        price += priceForCurrentDay;     
+    }
+    console.log(`Total: ${price.toFixed(2)} leva`);
+}
