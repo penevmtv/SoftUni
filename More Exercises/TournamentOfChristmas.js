7fa5190550da35d0fbf4cb8569092e2d40084d27
penevmtv <penevmtv@abv.bg>
2025-05-14 12:03:10 +0300

    com

--- More Exercises/TournamentOfChristmas.js
+++ More Exercises/TournamentOfChristmas.js
@@ -3,30 +3,30 @@ function solve(arr) {
   let index = 1;
   let allEarnedMoney = 0;
   let winingDays = 0;
-  let losesDays = 0;
+  let losingDays = 0;
   for (let currentDay = 1; currentDay <= days; currentDay++) {
     let earnedMoneyPerDay = 0;
     let winsForDay = 0;
-    let losesForDay = 0;
+    let lossesForDay = 0;
     while (arr[index] !== `Finish`) {
       let result = arr[index + 1];
       if (result === `win`) {
         winsForDay++;
         earnedMoneyPerDay += 20;
       } else 
-        losesForDay++;
+        lossesForDay++;
       index += 2; 
     }
     index++;
-    if (winsForDay > losesForDay) { 
+    if (winsForDay > lossesForDay) { 
       earnedMoneyPerDay *= 1.1;
       winingDays++;
     } else 
-      losesDays++;
+      losingDays++;
     
     allEarnedMoney += earnedMoneyPerDay;
   }
-  if (winingDays > losesDays) {
+  if (winingDays > losingDays) {
   allEarnedMoney *= 1.2;
   console.log(`You won the tournament! Total raised money: ${allEarnedMoney.toFixed(2)}`);
   } else 
