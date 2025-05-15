9d5ac7e8824f947fdf239ec2f0804df37956f4b5
penevmtv <penevmtv@abv.bg>
2025-05-15 17:28:55 +0300

    com

---
+++ More Exercises/basketballTournament.js
@@ -0,0 +1,26 @@
+function solve(arr) {
+  let index = 0;
+  let allMatchesCounter = 0;
+  let allWins = 0;
+  while(arr[index] !== `End of tournaments`) {
+    let nameOfTournament = arr[index];
+    let nMatches = Number(arr[index + 1]);
+    index += 2;
+    for(let currentMatch = 1; currentMatch <= nMatches; currentMatch++) {
+      allMatchesCounter++;
+      let myPoints = Number(arr[index]);
+      index++;
+      let otherPoints = Number(arr[index]);
+      let diff = Math.abs(myPoints - otherPoints);
+      if(myPoints > otherPoints) {
+        allWins++;
+        console.log(`Game ${currentMatch} of tournament ${nameOfTournament}: win with ${diff} points.`);
+      } else {
+        console.log(`Game ${currentMatch} of tournament ${nameOfTournament}: lost with ${diff} points.`);
+      }
+      index++;
+    }
+  }
+  let percentWins = (allWins / allMatchesCounter) * 100;
+  console.log(``)
+}
