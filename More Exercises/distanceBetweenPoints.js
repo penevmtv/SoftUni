1606359bfeadf2ed7db56b2fbe4f5c62bb08d0d1
penevmtv <penevmtv@abv.bg>
2025-05-20 15:53:14 +0300

    update distanceBetweenPoints.js

---
+++ More Exercises/distanceBetweenPoints.js
@@ -0,0 +1,6 @@
+function solve(x1, y1, x2, y2) {
+  let xDistance = Math.abs(x1 - x2);
+  let yDistance = Math.abs(y1 - y2);
+  let xYDistance = Math.sqrt(xDistance ** 2 + yDistance ** 2);
+  console.log(xYDistance);
+}
