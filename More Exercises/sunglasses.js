75520199df73c84c9a21708341b4841d447e5e1e
penevmtv <penevmtv@abv.bg>
2025-05-20 20:25:23 +0300

    com

--- More Exercises/sunglasses.js
+++ More Exercises/sunglasses.js
@@ -1,15 +1,17 @@
 function solve(n) {
   for (let currentRow = 1; currentRow <= n; currentRow++) {
+    let toPrint = ``;
     for (let currentColumn = 1; currentColumn <= n * 5; currentColumn++) {
-      if (currentColumn >  (n * 5 - n) / 2 &&  currentColumn <=  (n * 5 + n) / 2) {
-        if (currentRow !== Math.floor(n / 2)) 
-          continue;
-        else 
-          console.log(`|`);
+      if (currentColumn > (n * 5 - n) / 2 && currentColumn <= (n * 5 + n) / 2) {
+        if (currentRow !== Math.floor(n / 2))
+          toPrint += ` `;
+        else
+          toPrint += `|`;
       } else if ((currentRow === 1 || currentRow === n) || currentColumn === 1 || currentColumn === n * 5 || currentColumn === (n * 5 - n) / 2 || currentColumn === (n * 5 + n) / 2 + 1) {
-        console.log(`*`);
-      } else 
-        console.log(`/`);
+        toPrint += `*`;
+      } else
+        toPrint += `/`;
     }
+    console.log(toPrint);
   }
 }
