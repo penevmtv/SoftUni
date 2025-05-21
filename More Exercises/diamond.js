029bc23a93016eaa90256eca6de61d5df530ef9e
penevmtv <penevmtv@abv.bg>
2025-05-21 23:51:36 +0300

    com

--- More Exercises/diamond.js
+++ More Exercises/diamond.js
@@ -1,29 +1,28 @@
 function solve(n) {
-  let firstStar = Math.ceil(n % 2);
-  let secondStar = 0;
-  let allRows = n;
-  let isEvenN = false;
-  if (n % 2 === 0) {
-    allRows = n - 1;
-    secondStar = star + 1;
-    isEvenN = true;
-  }
-  for (let row = 1; row <= allRows; row++) {
-    let toPrint = ``;
-    for (let column = 1; column <= n; column++) {
-      if (row === 1 && column === firstStar) {
-        if (!isEvenN) { 
-          toPrint += `*`;
-          secondStar = firstStar + 1;
-        } else {
-          toPrint += `**`;
-          column++;
-          secondStar++;
-        }
-        firstStar--;
-      }
-      
+  let middleRow = Math.floor(n / 2);
+  let leftRight, mid;
+  for (let row = 0; row < n; row++) {
+    if (row <= middleRow) {
+      leftRight = (n - 1) / 2 - row;
+    } else {
+      leftRight = row - middleRow;
     }
+    mid = n - 2 * leftRight - 2;
+    let line = '';
+    for (let i = 0; i < leftRight; i++) {
+      line += '-';
+    }
+    line += '*';
+    if (mid >= 0) {
+      for (let i = 0; i < mid; i++) {
+        line += '-';
+      }
+      line += '*';
+    }
+    for (let i = 0; i < leftRight; i++) {
+      line += '-';
+    }
+
+    console.log(line);
   }
 }
-  
