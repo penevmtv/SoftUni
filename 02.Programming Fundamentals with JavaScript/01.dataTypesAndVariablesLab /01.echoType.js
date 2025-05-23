de0daf4d4a2a136d26ce10fc792d1c0a45e9126d
penevmtv <penevmtv@abv.bg>
2025-05-23 23:53:18 +0300

    com

--- 02.Programming Fundamentals with JavaScript/01.dataTypesAndVariablesLab /01.echoType.js
+++ 02.Programming Fundamentals with JavaScript/01.dataTypesAndVariablesLab /01.echoType.js
@@ -1,7 +1,7 @@
-/* Write a JS function that takes one parameter and prints on two lines the type of the parameter
- and then one of the following:
-•	If the parameter type is either string or number, print its value
-•	Otherwise, print the text 'Parameter is not suitable for printing' */
+//  Write a JS function that takes one parameter and prints on two lines the type of the parameter
+//  and then one of the following:
+// If the parameter type is either string or number, print its value
+// Otherwise, print the text 'Parameter is not suitable for printing' 
 
 
 function solve(parameter) {
