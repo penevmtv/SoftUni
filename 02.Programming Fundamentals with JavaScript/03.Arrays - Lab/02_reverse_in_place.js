308d154e74f2415aa61ab1ced8a054a03656f43b
penevmtv <penevmtv@abv.bg>
2025-05-29 18:39:04 +0300

    Create 02_reverse_in_place.js

---
+++ 02.Programming Fundamentals with JavaScript/03.Arrays - Lab/02_reverse_in_place.js
@@ -0,0 +1,14 @@
+// Write a program, which receives an array of strings. Your task is to reverse the array without creating a new array 
+// and without using ".reverse". Print the resulting elements on a single line, space-separated. 
+
+
+
+function solve(arr) {
+    for (let i = 0; i < arr.length / 2; i++) {
+        let firstHalfIndex = arr[arr.length - 1 - i];
+        let secondHalfIndex = arr[i];
+        arr[i] = firstHalfIndex;
+        arr[arr.length - 1 - i] = secondHalfIndex;
+    }
+    console.log(arr.join(` `));
+}
