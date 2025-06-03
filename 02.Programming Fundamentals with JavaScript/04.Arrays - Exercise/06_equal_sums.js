1a2415901b2e624bf89bbc5a5c84b1c3d295ec9a
penevmtv <penevmtv@abv.bg>
2025-06-03 20:48:16 +0300

    create 06_equal_sums.js

---
+++ 02.Programming Fundamentals with JavaScript/04.Arrays - Exercise/06_equal_sums.js
@@ -0,0 +1,32 @@
+//Write a function that determines if there exists an element in the array of numbers such that
+//the sum of the elements on its left is equal to the sum of the elements on its right. 
+
+//If there are NO elements to the left/right, their sum is 0. 
+
+//Print the index that satisfies the required condition or "no" if there is no such index.
+
+
+
+function solve(arr) {
+  let isEqual = false;
+  for (let i = 1; i < arr.length - 1; i++) {
+    let leftSum = 0;
+    for (let j = 0; j < i; j++) {
+      leftSum += arr[j];
+    }
+    let rightSum = 0;
+    for (let k = i + 1; k < arr.length; k++) {
+      rightSum += arr[k];
+    }
+    if (leftSum === rightSum) {
+      console.log(i);
+      isEqual = true;
+      break;
+    }
+  }
+  if (!isEqual && arr.length === 1) {
+    console.log(0);
+  } else if (!isEqual) {
+    console.log(`no`);
+  }
+}
