65507ada87c8f4077e0f66f206d8d3c79900a19d
penevmtv <penevmtv@abv.bg>
2025-07-30 13:48:39 +0300

    Create 08-travel-time.js

---
+++ 02.Programming Fundamentals with JavaScript/14.Map and Set - Exercise/08-travel-time.js
@@ -0,0 +1,24 @@
+// Write a function that collects and orders information about travel destinations.
+// As input, you will receive an array of strings.
+// Each string will consist of the following information with the format:
+// "Country name > Town name > Travel cost"
+
+// The Country name will be a string, the Town name will be a unique string, Travel cost will be a number.
+// If you receive the same Town name twice, you should keep the cheapest offer. Have in mind that one Country may have several Towns to visit.
+// After you finish the organizational part, you need to let Steven know which destination point to visit first. The order will be as follows:
+//   First sort Country names alphabetically and then sort by lowest Travel cost.
+
+
+
+
+function travelTime(arrOfStrs) {
+    
+}
+travelTime([
+"Bulgaria > Sofia > 500",
+"Bulgaria > Sopot > 800",
+"France > Paris > 2000",
+"Albania > Tirana > 1000",
+"Bulgaria > Sofia > 200"
+]
+);
