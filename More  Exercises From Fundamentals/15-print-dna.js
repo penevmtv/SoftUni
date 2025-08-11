0d17296df2983f1cf5d4e9d89bcc138f28ade5ff
penevmtv <penevmtv@abv.bg>
2025-08-11 13:34:19 +0300

    Create 15-print-dna.js

---
+++ More  Exercises From Fundamentals/15-print-dna.js
@@ -0,0 +1,12 @@
+// Write a JS program that prints a DNA helix with a length, specified by the user. The helix has a repeating structure,
+//  but the symbol in the chain follows the sequence ATCGTTAGGG.
+// The input comes as a single number. It represents the length of the required helix.
+// The output is the completed structure, printed on the console.
+
+
+
+
+function printDNA(numLength) {
+
+}
+printDNA(10);
