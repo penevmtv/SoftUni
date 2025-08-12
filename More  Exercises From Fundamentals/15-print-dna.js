52b72ff480598bfa0cd9415e46fc1420f741d934
penevmtv <penevmtv@abv.bg>
2025-08-12 11:06:00 +0300

    update 15-print-dna.js

--- More  Exercises From Fundamentals/15-print-dna.js
+++ More  Exercises From Fundamentals/15-print-dna.js
@@ -7,6 +7,26 @@
 
 
 function printDNA(numLength) {
-
+  const char = [`A`, `T`, `C`, `G`, `T`, `T`, `A`, `G`, `G`, `G`];
+  let firstCharIndex = 2;
+  let secondCharIndex = 3;
+  const star = `*`;
+  const dash = `-`;
+  let indexOfCharArr = 0;
+  let isSpread = true;
+  
+  for (let i = 0; i < numLength; i++) {
+    let lineToPrint = [];
+    
+    for (let j = 0; j < 6; j++) {
+      if (j < firstCharIndex || j > secondCharIndex) {
+        lineToPrint.push(star);
+      } else if (j === firstCharIndex) {
+        lineToPrint.push(char[indexOfCharArr]);
+        indexOfCharArr++;
+        if (isSpread)
+      }
+    }
+  }
 }
 printDNA(10);
