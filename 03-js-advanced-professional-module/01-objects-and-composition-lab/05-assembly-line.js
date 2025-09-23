2ae1acd38360378a5d918aa7a877bcf88e23e6c0
penevmtv <penevmtv@abv.bg>
2025-09-23 23:23:59 +0300

    update

--- 03-js-advanced-professional-module/01-objects-and-composition-lab/05-assembly-line.js
+++ 03-js-advanced-professional-module/01-objects-and-composition-lab/05-assembly-line.js
@@ -29,12 +29,66 @@
 
 
 
-function assemblyLine() {
+function createAssemblyLine() {
+    const libraryObj = {
+        hasClima(carObj) {
+            carObj.temp = 21;
+            carObj.tempSettings = 21;
 
+            carObj.adjustTemp = function () {
+                if (this.temp < this.tempSettings) this.temp++;
+                else if (this.temp > this.tempSettings) this.temp--;
+            }
+        },
+        hasAudio(carObj) {
+            carObj.currentTrack = {
+                name: 0,
+                artist: 0
+            }
+            carObj.nowPlaying = function () {
+                if (Object.values(this.currentTrack).every(value => value !== 0)) {
+                    console.log(`Now playing '${this.currentTrack.name}' by ${this.currentTrack.artist}`)
+                }
+            }
+        },
+        hasParktronic(carObj) {
+            carObj.checkDistance = function (distance) {
+                if (distance < 0.1) console.log(`Beep! Beep! Beep!`);
+                else if (distance < 0.25) console.log(`Beep! Beep!`);
+                else if (distance < 0.5) console.log(`Beep!`);
+                else console.log(``);
+            }
+        }
+    }
+    return libraryObj;
 }
+
+
+//Input: 
+
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
 
 assemblyLine.hasClima(myCar);
 console.log(myCar.temp);
 myCar.tempSettings = 18;
 myCar.adjustTemp();
 console.log(myCar.temp);
+
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
