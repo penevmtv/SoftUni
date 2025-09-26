ee0f60e40e173784e5693cf73eed15a1597a12e4
penevmtv <penevmtv@abv.bg>
2025-09-26 12:47:55 +0300

    Create 03-car-factory.js

---
+++ 03-js-advanced-professional-module/02-objects-and-composition-exercise/03-car-factory.js
@@ -0,0 +1,65 @@
+// Write a program that assembles a car by giving requirements out of existing components. 
+// The client will place an order in the form of an object describing the car. 
+// You need to determine which parts to use to fulfill the client’s order. You have the following parts in storage:
+// An engine has power (given in horsepower) and volume (given in cubic centimeters). 
+// Both of these values are numbers. When selecting an engine, pick the smallest possible that still meets the requirements.
+// Small engine: { power: 90, volume: 1800 }
+// Normal engine: { power: 120, volume: 2400 }
+// Monster engine: { power: 200, volume: 3500 }
+// A carriage has a type and color. Both of these values are strings. You have two types of carriages in storage and can paint them any color.
+// Hatchback: { type: 'hatchback', color: <as required> }
+// Coupe: { type: 'coupe', color: <as required> }
+// The wheels will be represented by an array of 4 numbers, each number represents the diameter of the wheel in inches. 
+// The size can only be an odd number. Round down any requirements you receive to the nearest odd number. 
+// Input
+// You will receive an object as an argument to your function. The format will be as follows:
+// { model: <model name>,
+//   power: <minimum power>,
+//   color: <color>,
+//   carriage: <carriage type>,
+//   wheelsize: <size> }
+// Output
+// Return the resulting car object as a result of your function. See the examples for details.
+
+
+
+
+function carFactory(orderObj) {
+
+    function engineSelecting(neededPower) {
+        const typesEnginesObj = {
+            smallEngine: { power: 90, volume: 1800 },
+            normalEngine: { power: 120, volume: 2400 },
+            monsterEngine: { power: 200, volume: 3500 }
+        }
+        if (neededPower <= 90) return typesEnginesObj.smallEngine;
+        if (neededPower <=120) return typesEnginesObj.normalEngine;
+        return typesEnginesObj.monsterEngine;
+    }
+
+    function carriageSelecting(type, color) {
+        if (type === `hatchback` || type === `coupe`) return { type, color };
+    }
+
+    function wheelsSelecting(size) {
+        const oddNumber = size % 2 !== 0 ? size : size - 1;
+
+        return new Array(4).fill(oddNumber);
+    }
+
+    const carObj = {};
+
+    carObj.model = orderObj.model;
+    carObj.engine = engineSelecting(orderObj.power);
+    carObj.carriage = carriageSelecting(orderObj.carriage, orderObj.color);
+    carObj.wheels = wheelsSelecting(orderObj.wheelsize);
+
+    return carObj;
+}
+carFactory({
+  model: 'Opel Vectra',
+  power: 110,
+  color: 'grey',
+  carriage: 'coupe',
+  wheelsize: 17 
+});
