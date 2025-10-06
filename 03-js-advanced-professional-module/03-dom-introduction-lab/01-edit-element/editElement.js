87619cd1cf8c572c2091a1d65394e2a7c605fb57
penevmtv <penevmtv@abv.bg>
2025-10-06 19:03:54 +0300

    Update editElement.js

--- 03-js-advanced-professional-module/03-dom-introduction-lab/01-edit-element/editElement.js
+++ 03-js-advanced-professional-module/03-dom-introduction-lab/01-edit-element/editElement.js
@@ -1,3 +1,11 @@
+Create function edit() that takes three parameters.
+
+Input / Output
+The first parameter is a reference to an HTML element, the other two parameters are string–match and replacer.
+You have to replace all occurrences of the match inside the text content of the given element with a replace
+
+
+
 function editElement(ref, match, replacer) {
     ref.textContent = ref.textContent.replaceAll(match, replacer);
 };
