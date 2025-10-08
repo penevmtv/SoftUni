f4bfce7b53b77b694bd5d961796cbbd4f82e2801
penevmtv <penevmtv@abv.bg>
2025-10-08 11:34:21 +0300

    add problems condition

--- 03-js-advanced-professional-module/03-dom-introduction-lab/03-sum-numbers/sum-numbers.js
+++ 03-js-advanced-professional-module/03-dom-introduction-lab/03-sum-numbers/sum-numbers.js
@@ -1,3 +1,11 @@
+// Write a JS function that reads two numbers from input fields in a web page and puts their
+//  sum in another field when the user clicks on a button.
+// Input / Output
+// There will be no Input / Output, your program should instead modify the DOM of the given HTML document.
+
+
+
+
 function calc() {
     const firstNum = Number(document.querySelector('#num1').value);   
     const secondNum = Number(document.querySelector('#num2').value); 
--- 03-js-advanced-professional-module/03-dom-introduction-lab/04-show-more/ShowMore.js
+++ 03-js-advanced-professional-module/03-dom-introduction-lab/04-show-more/ShowMore.js
@@ -1,3 +1,11 @@
+// Write a JS function that expands a hidden section of text when a link is clicked. 
+// The link should disappear as the rest of the text shows up.
+// Input / Output
+// There will be no Input / Output, your program should instead modify the DOM of the given HTML document.
+
+
+
+
 function showText() {
   document.querySelector('#more').style.display = 'none';
     document.querySelector('#text').style.display = 'inline';
