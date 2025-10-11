08511faee5f4cf57fbde754c7e503d239fcfd0bd
penevmtv <penevmtv@abv.bg>
2025-10-11 18:46:20 +0300

    create 01-subtraction

---
+++ 03-js-advanced-professional-module/04-dom-introduction-exercise/01-subtraction/subtract.js
@@ -0,0 +1,9 @@
+function subtract() {
+    const firstNum = Number(document.getElementById('firstNumber').value);
+    const secondNum = Number(document.getElementById('secondNumber').value);
+    let result = document.getElementById('result');
+
+    result.textContent = firstNum - secondNum;
+
+    console.log(result);
+}
